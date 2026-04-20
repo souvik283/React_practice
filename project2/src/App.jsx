8d3423155a7390f5b4d27a215f11920075af1f29
Souvik Ghosh <ghosh2005202318@gmail.com>
2026-04-21 01:23:35 +0530

    edit

---
+++ project2/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from './components/card'
+import Card2 from './components/card2'
+
+const App = () => {
+  return (
+    <div className='parent'>
+      {/* <Card user="Souvik" age={18} img="https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg" alt="profile photo"/> */}
+      {/* <Card user="Ghosh" age= {21} img="https://img.freepik.com/free-photo/closeup-shot-beautiful-butterfly-with-interesting-textures-orange-petaled-flower_181624-7640.jpg?semt=ais_hybrid&w=740&q=80"/> */}
+    <Card2/>
+    </div>
+  )
+}
+
+export default App
